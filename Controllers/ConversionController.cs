f766136cd90558beae386a28c64edcfa64a9fa9f
Olatundeafeez <olatundefolorunshoafeez@gmail.com>
2024-07-04 20:39:51 +0100

    third commit

---
+++ Controllers/ConversionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Temparature_Converter.Service;
+
+namespace Temparature_Converter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversionController : ControllerBase
+    {
+        [HttpGet(nameof(ConvertFahrenheitToCelcius))]
+        public IActionResult ConvertFahrenheitToCelcius(double fahrenheit)
+        {
+            var far = TemperatureConversion.Fahrenheit(fahrenheit);
+            return Ok(far);
+
+        }
+
+        [HttpGet(nameof(ConvertCelciusToFahrenheit))]
+        public IActionResult ConvertCelciusToFahrenheit(double Celcius)
+        {
+            var cel = TemperatureConversion.celsius(Celcius);
+            return Ok(cel);
+          
+        }
+
+    }
+}
---
+++ Service/TemperatureConversion.cs
@@ -0,0 +1,18 @@
+﻿namespace Temparature_Converter.Service
+{
+    public class TemperatureConversion
+    {
+        public static double Fahrenheit(double f)
+        {
+            var c = (f - 32) * 5 / 9;
+            return c;
+
+        }
+        public static double celsius(double c)
+        {
+            var f = (c * 9 / 5) + 32;
+            return f;
+            
+        }
+    }
+}
